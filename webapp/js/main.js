fdcbbd8bc7758c558026e8d33d18caf6a4e09af6
miguelgazela <miguel.gazela@gmail.com>
2013-11-01 16:55:41 +0000

    brand color added to navbar

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,8 +1,22 @@
 var BASE_PATH = "http://172.30.27.137:8888/sinf/";
 var BASE_PATH2 = "http://localhost:8888/sinf/";
 
+var sinfapp = sinfapp || {};
+
 $(document).ready(function(){
     console.log("Page ready!");
+
+    sinfapp.ui = {
+        sidebar_btns: $('#features ul > .sidebar-btn')
+    };
+
+
+    sinfapp.ui.sidebar_btns.each(function(){
+        console.log("Loading "+$(this).attr('data-section'));
+        $(this).click(function(){
+            console.log("Loading man!");
+        });
+    });
 });
 
 Lungo.dom("#clients").on("load", function(){
