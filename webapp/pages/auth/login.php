732e5b88a8232fbd5c140e1393e376f615c57833
nelsonmendes <nelson.daniel.mendes@gmail.com>
2013-11-19 13:52:04 +0000

    change file name from login.html to login.tpl, thats why that didnt work as intended

--- webapp/pages/auth/login.php
+++ webapp/pages/auth/login.php
@@ -2,12 +2,12 @@
     // initialize
     include_once('../../common/init.php');
 
-    if(isset($_SESSION['session_id'])) {
+    /*if(isset($_SESSION['session_id'])) {
         header("Location: $BASE_URL"."encomendas/ativas");
         die();
     }
 
-    die("You need to login");
+    die("You need to login");*/
 
     // display smarty template
     $smarty->display('auth/login.tpl');
