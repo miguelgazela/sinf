c18a47c3687a26fa0ce06738c280e3ce293c57a7
nelsonmendes <nelson.daniel.mendes@gmail.com>
2013-11-25 11:11:58 +0000

    Enable cross domain file

--- webapp/common/init.php
+++ webapp/common/init.php
@@ -1,6 +1,6 @@
 <?php
     // This file is included in every page
-    //ini_set('display_errors', 'On');
+    ini_set('display_errors', 'On');
 
     // production paths
     //$BASE_URL = 'http://paginas.fe.up.pt/~ei10076/projects/agiba/';
@@ -8,9 +8,9 @@
 
     // development paths
     
-    $BASE_URL = "http://localhost/sinf/webapp/";
+    $BASE_URL = "http://localhost:8888/sinf/webapp/";
     //$BASE_PATH = '/Users/migueloliveira/Dropbox/projects/sinf/webapp/';
-    // $BASE_PATH = '/Users/nelsonmendes/Documents/Git/sinf/webapp/';
+    $BASE_PATH = '/Users/nelsonmendes/Documents/Git/sinf/webapp/';
 
     // Slim Framework
     include_once ($BASE_PATH .'libs/Slim/Slim.php');
